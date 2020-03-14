f6cda5b940427ce1b0c0049415beb5cd9eb6f226
jimmy <tv2169145@gmail.com>
2020-03-15 02:03:22 +0800

    add interface for rest error

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,47 +1,92 @@
 package rest_errors
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 )
 
-type RestErr struct {
-	Message string `json:"message"`
-	Status int `json:"status"`
-	Error string `json:"error"`
-	Causes []interface{} `json:"causes"`
+type RestErr interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []interface{}
 }
+
+type restErr struct {
+	ErrMessage string `json:"message"`
+	ErrStatus int `json:"status"`
+	ErrError string `json:"error"`
+	ErrCauses []interface{} `json:"causes"`
+}
+
+func (e *restErr) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: %v", e.ErrMessage, e.ErrStatus, e.ErrError, e.ErrCauses)
+}
+
+func (e *restErr) Message() string {
+	return e.ErrMessage
+}
+
+func (e *restErr) Status() int {
+	return e.ErrStatus
+}
+
+func (e *restErr) Causes() []interface{} {
+	return e.ErrCauses
+}
+
+func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
+	return &restErr{
+		ErrMessage: message,
+		ErrStatus:  status,
+		ErrError:   err,
+		ErrCauses:  causes,
+	}
+}
+
+func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
+	var apiErr *restErr
+	if err := json.Unmarshal(bytes, &apiErr); err != nil {
+		return nil, errors.New("invalid json")
+	}
+	return apiErr, nil
+}
+
 
 func NewError(msg string) error {
 	return errors.New(msg)
 }
 
-func NewBadRequestError(message string) *RestErr  {
-	result := &RestErr{
-		Message: message,
-		Status: http.StatusBadRequest,
-		Error: "bad request",
+
+
+func NewBadRequestError(message string) RestErr  {
+	result := &restErr{
+		ErrMessage: message,
+		ErrStatus: http.StatusBadRequest,
+		ErrError: "bad request",
 	}
 	return result
 }
 
-func NewNotFoundError(message string) *RestErr {
-	result := &RestErr{
-		Message: message,
-		Status: http.StatusNotFound,
-		Error: "not found",
+func NewNotFoundError(message string) RestErr {
+	result := &restErr{
+		ErrMessage: message,
+		ErrStatus: http.StatusNotFound,
+		ErrError: "not found",
 	}
 	return result
 }
 
-func NewInternalServerError(message string, err error) *RestErr {
-	result := &RestErr{
-		Message: message,
-		Status: http.StatusInternalServerError,
-		Error: "internal server error",
+func NewInternalServerError(message string, err error) RestErr {
+	result := &restErr{
+		ErrMessage: message,
+		ErrStatus: http.StatusInternalServerError,
+		ErrError: "internal server error",
 	}
 	if err != nil {
-		result.Causes = append(result.Causes, err.Error())
+		result.ErrCauses = append(result.ErrCauses, err.Error())
 	}
 	return result
 }
