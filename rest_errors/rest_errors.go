39743d7bae32d47ce921a67524ff1d664c6579bc
jimmy <tv2169145@gmail.com>
2020-03-22 01:08:17 +0800

    edit something

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -15,14 +15,14 @@ type RestErr interface {
 }
 
 type restErr struct {
-	ErrMessage string `json:"message"`
-	ErrStatus int `json:"status"`
-	ErrError string `json:"error"`
-	ErrCauses []interface{} `json:"causes"`
+	ErrMessage string        `json:"message"`
+	ErrStatus  int           `json:"status"`
+	ErrError   string        `json:"error"`
+	ErrCauses  []interface{} `json:"causes"`
 }
 
 func (e *restErr) Error() string {
-	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: %v", e.ErrMessage, e.ErrStatus, e.ErrError, e.ErrCauses)
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: [ %v ]", e.ErrMessage, e.ErrStatus, e.ErrError, e.ErrCauses)
 }
 
 func (e *restErr) Message() string {
@@ -54,18 +54,15 @@ func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
 	return apiErr, nil
 }
 
-
 func NewError(msg string) error {
 	return errors.New(msg)
 }
 
-
-
-func NewBadRequestError(message string) RestErr  {
+func NewBadRequestError(message string) RestErr {
 	result := &restErr{
 		ErrMessage: message,
-		ErrStatus: http.StatusBadRequest,
-		ErrError: "bad request",
+		ErrStatus:  http.StatusBadRequest,
+		ErrError:   "bad request",
 	}
 	return result
 }
@@ -73,8 +70,8 @@ func NewBadRequestError(message string) RestErr  {
 func NewNotFoundError(message string) RestErr {
 	result := &restErr{
 		ErrMessage: message,
-		ErrStatus: http.StatusNotFound,
-		ErrError: "not found",
+		ErrStatus:  http.StatusNotFound,
+		ErrError:   "not found",
 	}
 	return result
 }
@@ -82,16 +79,16 @@ func NewNotFoundError(message string) RestErr {
 func NewUnauthorizedError(message string) RestErr {
 	return &restErr{
 		ErrMessage: message,
-		ErrStatus: http.StatusUnauthorized,
-		ErrError: "Unauthorized",
+		ErrStatus:  http.StatusUnauthorized,
+		ErrError:   "Unauthorized",
 	}
 }
 
 func NewInternalServerError(message string, err error) RestErr {
 	result := &restErr{
 		ErrMessage: message,
-		ErrStatus: http.StatusInternalServerError,
-		ErrError: "internal server error",
+		ErrStatus:  http.StatusInternalServerError,
+		ErrError:   "internal server error",
 	}
 	if err != nil {
 		result.ErrCauses = append(result.ErrCauses, err.Error())
